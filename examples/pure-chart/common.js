39d91f69c81777219919b2da5a603487201ea96e
nareshghule-tudip <52404174+nareshghule-tudip@users.noreply.github.com>
2021-12-15 20:05:24 +0530

    Update common.js

--- examples/pure-chart/common.js
+++ examples/pure-chart/common.js
@@ -244,7 +244,7 @@ export const drawYAxisLabels = (arr, height, minValue, color = '#000000', symbol
   return (
     <View style={{
       width: 33 + 5*symbol.length,
-      height: height,
+      height: height +5,
       justifyContent: 'flex-end',
       alignItems: 'flex-end',
       marginBottom: minValue && arr && arr.length > 0 ? -1 * arr[0][2] * minValue : null,
